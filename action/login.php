8c2ed67c3f6792e09faf5c2bfdd498ddf33dba24
minhtn143 <minhtn143@gmail.com>
2020-04-27 00:33:04 +0700

    pending check email pw

--- action/login.php
+++ action/login.php
@@ -4,5 +4,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = $_REQUEST['password'];
 
     $userManager = new UserManager("../data/data.json");
+/*pending
+lay mail trong data kiem tra trung voi email nhap vao ko
+Neu k thi thong bao sai email
+Neu co check tiep co trung pass ko
+    neu khong thi thong bao sai pw
+    neu dung thi ok login
 
+*/
 }
